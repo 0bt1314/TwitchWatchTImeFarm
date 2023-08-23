6bd598de925e735a2d3e36b875f1487e63892355
Fabian <62375683+vlc3n@users.noreply.github.com>
2023-08-23 12:16:14 +0200

    Update run.py

--- run.py
+++ run.py
@@ -2,34 +2,29 @@ import requests
 import subprocess
 import time
 import os
-from time import sleep
 
 # Input your "Target"
 channelName = 'USER'
 
-contents = requests.get('https://www.twitch.tv/' + channelName).content.decode('utf-8')
+# Twitch Chat URL
+chat_url = f'https://www.twitch.tv/{channelName}/chat'
 
-refreshingalways = True
+# Function to check if the stream is live
 def checkStream():
-    while refreshingalways == True:
-        if 'isLiveBroadcast' in contents:
-            # Return True if the Streamer is streaming
-            print(channelName + " is now live") 
-            time.sleep(1800)
-            return True
-        else:
-            # Return False if the Streamer is not streaming
-            print(channelName + " is not live")
-            time.sleep(1800)
-            return False
+    response = requests.get(chat_url)
+    if response.status_code == 200:
+        return True
+    else:
+        return False
 
 def startBot():
-    # If checkStream() outputs True, meaning the Streamer is Streaming, then we will open a Chromium Browsers with just the Chat
-    if checkStream() == True:
-        with open (os.devnull, "w") as fp:
-            url = "https://www.twitch.tv/popout/" + channelName + "/chat"
-            # Creating and using the Process
-            p = subprocess.Popen(["chromium-browser", url], stdout=fp, stderr=fp)
-            # If the Streamer goes offline agian, we want to close the Chat again.
-            if checkStream() == False:
-                p.kill()
+    while True:
+        if checkStream():
+            print(f'{channelName} is now live')
+                # TODO
+        else:
+            print(f'{channelName} is not live')
+        time.sleep(300)  # Check every 5 minutes
+if __name__ == "__main__":
+    startBot()
+    
